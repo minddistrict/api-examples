55636d88e8785fa0075840f37203d677c0d3dac7
nielsbom <nbom@minddistrict.com>
2015-07-27 22:53:44 +0200

    Add some checks around API url for PHP example.

--- php/settings.php
+++ php/settings.php
@@ -6,6 +6,16 @@ error_reporting(E_ALL);
 include "util.php";
 
 $config = parse_ini_file('config.ini');
-$config_url = $config['url'];
 $config_token = $config['token'];
 $config_path = $config['path'];
+$config_url = $config['url'];
+
+
+if (strpos($config_url, 'api' == False)) {
+    exit('The API url is incorrect; it does not contain "api".');
+}
+
+# Double parentheses to prevent the "passed by reference warning".
+if (end((str_split($config_url))) != '/') {
+    exit('The API url needs to end with a "/" character.');
+}
